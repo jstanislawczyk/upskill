c109325e1bd79dde4d440806610567dac3e01d03
jstanislawczyk <jakubstanislawczyk98@gmail.com>
2023-07-02 20:42:20 +0200

    Example Terragrunt

---
+++ 035/sprint-4/FirstService/lambda.tf
@@ -0,0 +1,45 @@
+data "archive_file" "lambda_zip_inline" {
+  type        = "zip"
+  output_path = "/tmp/lambda_zip_inline.zip"
+
+  source {
+    content  = <<EOF
+module.exports.handler = async (event, context, callback) => {
+	const what = "world";
+	const response = `Hello $${what}!`;
+    console.log(response);
+	callback(null, response);
+};
+EOF
+    filename = "main.js"
+  }
+}
+
+resource "aws_lambda_function" "lambda_zip_inline" {
+  function_name    = "jstanislawczyk-lambda-1"
+  handler          = "exports.handler"
+  filename         = data.archive_file.lambda_zip_inline.output_path
+  source_code_hash = data.archive_file.lambda_zip_inline.output_base64sha256
+  runtime          = "nodejs16.x"
+  role             = aws_iam_role.iam_for_lambda_1.arn
+}
+
+resource "aws_iam_role" "iam_for_lambda_1" {
+  name = "iam_for_lambda_1"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
